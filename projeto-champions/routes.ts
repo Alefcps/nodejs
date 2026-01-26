20c5ab91935e9d87fb1480e3d45bb4110a84af00
alef rodrigo <alef.rodrigo@outlook.com>
2026-01-26 17:09:05 -0300

    api-champions

---
+++ projeto-champions/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import * as PlayerController from "./src/controllers/players-controller";
+
+const router = Router();
+
+router.get("/players", PlayerController.getPlayer);
+router.post("/players", PlayerController.postPlayer);
+router.delete("/players/:id", PlayerController.deletePlayer);
+router.patch("/players/:id", PlayerController.updatePlayer);
+router.get("/players/:id", PlayerController.getPlayerById);
+
+
+
+export default router;
